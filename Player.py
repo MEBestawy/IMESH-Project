952aa4def8970681095c6fa1efb8ab65e67b249b
Herjot <herjot18@outlook.com>
2019-10-27 17:10:28 -0400

    Add icon to player class

--- Player.py
+++ Player.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+import pygame
+
 class Player:
     """
     This class represents one of the two players that will play
@@ -7,12 +10,16 @@ class Player:
 
     disc: the disc that Player uses when making a move.
 
+    icon: the image representing this player
+
     """
-
+    
     disc: str
+    icon: pygame.Surface
 
-    def __init__(self, disc):
+    def __init__(self, disc: str, icon_file: str):
         """
-        Instantiate a Player object.
+        Instantiate a Player object, with their respective
         """
         self.disc = disc
+        self.icon = pygame.image.load(icon_file)
