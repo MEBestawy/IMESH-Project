7d1ef31a73a7e9a29a22e2b0b18af75efe893ee2
yongelys <elysia.yong@mail.utoronto.ca>
2019-10-27 15:50:45 -0400

    Create State Class for the purpose of consistent code reference
    Purpose of the State Class is as described in the .py file. This class enumerates different game states such that there is consistency in calling the game states in different classes.

---
+++ State.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class STATE(Enum):
+    """
+    This class is an enumeration of different game states settings and the creation
+    of this class is purely for coding consistency.
+
+    All variables in this class are static.
+    """
+    # Public Static Variables
+    Menu = 1
+    About = 2
+    Game = 3
+    End = 4
+
