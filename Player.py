5abec55f9be487ffe135ff096deba2d4cb4a6359
Herjot <herjot18@outlook.com>
2019-10-27 20:49:43 -0400

    Add has won method and follow PEP8

--- Player.py
+++ Player.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import pygame
 
+
 class Player:
     """
     This class represents one of the two players that will play
@@ -16,19 +17,27 @@ class Player:
 
     disc: str
     icon: pygame.Surface
+    win_count: int
 
-    def __init__(self, disc: str, icon_file: str):
+    def __init__(self, disc: str, icon_file: str) -> None:
         """
         Instantiate a Player object, with their respective
         """
         self.disc = disc
         self.icon = pygame.image.load(icon_file)
 
-    def make_move(self, col: int):
+    def make_move(self, col: int) -> None:
         """
         Let self make a move at column <col>.
 
         :param col: a column in the game board.:
         :return:
+        """
+        raise NotImplementedError
+
+    def has_won(self) -> bool:
+        """
+        Determine whether the player has won the game
+        :return: True or false based on whether the player has won the game
         """
         raise NotImplementedError
