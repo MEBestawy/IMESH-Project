61f5f0105b32073614d065818d635ed70e8c3c1e
yongelys <elysia.yong@mail.utoronto.ca>
2019-11-04 09:59:27 -0500

    Change mousepress handling
    Switched from directly detecting mousepress to detecting mousepress via pygame's event handling.
    
    This approach prevents users from spamming an action by holding down the mouse button.

--- DisplayBoard.py
+++ DisplayBoard.py
@@ -14,97 +14,90 @@ LIGHTERBLUE = (217, 233, 239)
 
 class DisplayBoard:
     """
-    Connect 2^2 main game visualizer. 
-    
+    Connect 2^2 main game visualizer.
     Acts as the controller to Board.py, and updates Game.py
     """
-
     def __init__(self, game, handler, board):
         self.game = game
         self.handler = handler
         self.buttons = []
-        
+
         self.board = board
-    
 
     def on_event(self, mousepress):
-        
-        #print(pygame.mouse.get_pos())
-
+        # print(pygame.mouse.get_pos())
         selected_column = self.get_column(mousepress)
-        
+
         if selected_column >= 0:
             print("Selected column: " + str(selected_column))
 
     def tick(self):
-        if pygame.mouse.get_pressed()[0]:
-            self.on_event(pygame.mouse.get_pos())
+        for event in self.game.events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.on_event(pygame.mouse)
 
     def render(self, display):
-        
         # CHANGING THE GAME BACKGROUND
         display.fill((255, 255, 255))
-        
+
 
         SLOTSIZE = 75
         HOLE_SIZE = int(SLOTSIZE / 2) - 10
-        
+
         NUMBEROFCOLUMNS = self.board.get_grid_size()[1]
         NUMBEROFROWS = self.board.get_grid_size()[0]
-        
+
         # Adding a Background
         board_background = pygame.image.load('./Assets/BOARD.png').convert()
-        display.blit(board_background, (0, 0)) 
+        display.blit(board_background, (0, 0))
 
         # Creates the pyGame Board with corresponding slots and holes from the matrix board representation.
         # This strategy for creating the board was inspired by a tutorial on freeCodeCamp.org.
         # Video URL: https://www.youtube.com/watch?v=XpYz-q1lxu8
-        
+
         for column in range(NUMBEROFCOLUMNS):
-            
+
             for row in range(NUMBEROFROWS):
-                
-                pygame.draw.circle(display, 
-                                   LIGHTERBLUE, 
-                                   (190 + (column * (SLOTSIZE-5)), 122 + (row * (SLOTSIZE-5))), 
+
+                pygame.draw.circle(display,
+                                   LIGHTERBLUE,
+                                   (190 + (column * (SLOTSIZE-5)), 122 + (row * (SLOTSIZE-5))),
                                     HOLE_SIZE)
-                
-                
+
     def get_column(self, mousepress):
-        
-        position = mousepress[0]
- 
+        # Will need to refactor this.
+        position = mousepress.get_pos()[0]
+
         if 161 <= position <= 217:
             return 0
-        
-        elif 231 <= position <=  287:
+
+        elif 231 <= position <= 287:
             return 1
-        
-        elif 302 <= position <=  358:
+
+        elif 302 <= position <= 358:
             return 2
-        
-        elif 371 <= position <=  427:
+
+        elif 371 <= position <= 427:
             return 3
-        
-        elif 441 <= position <=  497:
+
+        elif 441 <= position <= 497:
             return 4
-        
-        elif 511 <= position <=  567:
+
+        elif 511 <= position <= 567:
             return 5
-        
-        elif 581 <= position <=  637:
+
+        elif 581 <= position <= 637:
             return 6
-         
+
         else:
             return -1
-            
-            
-                
-                
-                
-                
-                
-                
-                
-                
-                
+
+
+
+
+
+
+
+
+
+
